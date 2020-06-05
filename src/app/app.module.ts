65b8c1910eb0f1c4b9a25c2605fa9c3f7f2f6720
Jaap Forsman <jaap.forsman@ns.nl>
2020-06-05 13:34:58 +0200

    Restructuring + Formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientInMemoryWebApiModule, InMemoryDbService } from 'angular-in-memory-web-api';
-import { InMemoryDataService } from './in-memory-data.service';
-// import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HeroService } from './hero.service';
-import { DashboardComponent } from './dashboard.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroSearchComponent } from './hero-search.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+import { HeroService } from './services/hero.service';
+import { HeroesComponent } from './heroes/heroes.component';
+import { InMemoryDataService } from './services/in-memory-data.service';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule,
-    HttpClientModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
-      dataEncapsulation: false,
-      delay: 300,
-      passThruUnknownUrl: true
-    })
-  ],
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    HeroSearchComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-  ],
-  providers: [HeroService],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
+            dataEncapsulation: false,
+            delay: 300,
+            passThruUnknownUrl: true
+        })
+    ],
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        HeroSearchComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+    ],
+    providers: [HeroService],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/hero-search/hero-search.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+import { Hero } from '../models/hero.model';
+
+@Injectable()
+export class HeroSearchService {
+    constructor(private http: HttpClient) {
+    }
+
+    search(term: string): Observable<Hero[]> {
+        return this.http
+            .get<Hero[]>(`app/heroes/?name=${term}`)
+            .pipe(catchError(this.handleError));
+    }
+
+    private handleError(res: HttpErrorResponse) {
+        console.error(res.error);
+        return observableThrowError(res.error || 'Server error');
+    }
+}
